3cccb8adba2ffe331ea9b3f9dae56579974f905f
MANOJ <manojborra@ebsco.com>
2025-03-18 23:05:19 +0530

    Python scripting

---
+++ 2.Strings.py
@@ -0,0 +1,13 @@
+#https://www.w3schools.com/python/python_ref_string.asp
+
+aws_arn = 'arn:aws:iam::442426885544:user/Devops'
+aws_arn.upper()
+aws_arn.lower()
+aws_arn.split()  => ['arn:aws:iam::442426885544:user/Devops']
+aws_arn.split(:) => ['arn', 'aws', 'iam', '', '442426885544', 'user/Devops']
+aws_arn.split(':') => ['arn', 'aws', 'iam', '', '442426885544', 'user/Devops']
+aws_arn.split('/') => ['arn:aws:iam::442426885544:user', 'Devops']
+aws_arn.split('/')[0] => arn:aws:iam::442426885544:user
+aws_arn.split('/')[1]  => Devops
+aws_arn.replace('aws', 'azure') => arn:azure:iam::442426885544:user/Devops
+aws_arn.find('aws') => 4
